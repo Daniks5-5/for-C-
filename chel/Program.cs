895fc8941887bfc02b7a2d4ea147dc1814c48277
unknown <ssanym211@gmail.com>
2023-01-16 21:37:52 +0300

    new commit

---
+++ chel/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите своё имя: ");
+string username = Console.ReadLine();
+ if(username.ToLower() == "маша" )
+{
+ Console.WriteLine("Ура, это же Маша");
+}
+ else
+ {
+  Console.Write("Привет,");
+  Console.Write(username);  
+ }
